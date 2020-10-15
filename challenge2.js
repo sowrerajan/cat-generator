245036c106749ac038aa502dbf74cdf103b52455
sowrerajan <70574891+sowrerajan@users.noreply.github.com>
2020-10-15 22:36:37 +0530

    Add files via upload

---
+++ challenge2.js
@@ -0,0 +1,7 @@
+function generateCat() {
+  var image = document.createElement("img");
+  var div = document.getElementById("flex-cat-gen");
+  image.src =
+    "https://media1.tenor.com/images/e4d613ee59e79b93e38ec2521cce19e4/tenor.gif?itemid=5751430";
+  div.appendChild(image);
+}
